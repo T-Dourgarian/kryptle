6aa3a0982e85e04ed05b399a0bc6684d00a151a3
Thomas Dourgarian <thomasdourgarian@gmail.com>
2023-10-31 17:43:20 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const cors = require('cors');
+const cron = require('node-cron');
+const app = express();
+
+
+
+
+// Body parser middleware
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let numbersToUse = [5, 14, 5, 22, 17];
+let targetNumber = 5;
+
+function generateRandomIntegers(min, max, count) {
+    const randomIntegers = [];
+    for (let i = 0; i < count; i++) {
+        const randomInteger = Math.floor(Math.random() * (max - min + 1)) + min;
+        randomIntegers.push(randomInteger);
+    }
+    return randomIntegers;
+}
+
+function generateKryptoNumbers() {
+    const kryptoNumbers = generateRandomIntegers(1, 25, 6);
+    numbersToUse = kryptoNumbers.slice(0, 5);
+    targetNumber = kryptoNumbers[5];
+}
+
+// Schedule the function to run every day at midnight (00:00)
+cron.schedule('0 0 * * *', () => {
+    generateKryptoNumbers();
+}, {
+    timezone: 'America/Chicago' // Replace 'Your_Timezone_Here' with your server's timezone (e.g., 'America/New_York')
+});
+
+
+
+app.get('/dailykrypto',(req,res) => {
+	try {
+        res.json({
+            numbersToUse,
+            targetNumber
+        })
+    } catch(error) {
+        console.log(error);
+    }
+})
+
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,22 +1,187 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Mexp from 'math-expression-evaluator';
+import axios from 'axios';
 
 function App() {
+
+  const [numSet, setNumSet] = useState([]);
+  const [numbersUsed, setNumbersUsed] = useState([]);
+  const [target, setTarget] = useState('');
+  const [equation, setEquation] = useState('');
+  const [solution, setSolution] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [validSolutions, setValidSolutions] = useState([]);
+
+  const [seconds, setSeconds] = useState(0);
+
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  const formattedTime = `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  
+
+  useEffect(() => {
+    
+    gatherTodaysNumbers();
+
+
+    const interval = setInterval(() => {
+      setSeconds(prevSeconds => prevSeconds + 1);
+    }, 1000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(()=>{
+    if (errorMessage) {
+      setTimeout(function() {
+      setErrorMessage('')
+          }, 3000);
+        }
+    },
+   [errorMessage])
+
+  const mexp = new Mexp();
+
+  const numbersRE = /\b\d+\b/g;
+
+  const handleCalculate = () => {
+    try {
+      setSolution(mexp.eval(equation))
+    } catch (error) {
+      // setSolution('Invalid mathamatical expression');
+    }
+  }
+
+  useEffect(() => {
+    handleCalculate();
+    if (equation === '') {
+      setSolution('');
+    }
+
+  }, [equation])
+
+  const gatherTodaysNumbers = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/dailykrypto`);
+
+      const { data } = response;
+
+      if (data && data.numbersToUse && data.targetNumber) {
+         setNumSet(data.numbersToUse);
+         setTarget(data.targetNumber);
+      }
+
+
+    } catch(error) {
+      console.log(error);
+      setNumSet(['4', '9', '23', '5', '22'])
+      setTarget('6');
+    }
+  }
+
+
+  const validate = () => {
+
+    // checks that solution is a valid mathamatical equation
+    try {
+      setSolution(mexp.eval(equation));
+    } catch(error) {
+      setErrorMessage('Invalid mathamatical equation');
+      return false;
+    }
+
+    let numsUsed = equation.match(numbersRE);
+    
+    if (!numsUsed) return false;
+
+    // checks if solution uses more or less that 5 numbers
+    if (numsUsed.length > 5) {
+      setErrorMessage('Invalid: There are too many numbers in your solution');
+      return false;
+    } else if (numsUsed.length < 5) {
+      setErrorMessage('Invalid: You must use all 5 numbers individually');
+      return false;
+    }
+
+    // checks that solution uses correct 5 numbers
+    for (let i = 0; i < numSet.length; i++) {
+      if (!numSet.includes(numsUsed[i])) {
+        setErrorMessage(`Invalid: ${numsUsed[i]} is not a valid number`);
+        return false;
+      }
+    }
+
+    // checks that solution is correct
+    if (Number(target) !== Number(solution)) {
+      setErrorMessage(`Invalid: This solution does not equal ${target}`);
+      return false;
+    }
+
+    // checks if solution is a duplicate
+    if (validSolutions.includes(equation)) {
+      setErrorMessage(`Invalid: You have already found this solution`);
+      return false
+    }
+    setValidSolutions([...validSolutions, `${equation} = ${target} | ${formattedTime}`]);
+    setEquation('');
+    return true;
+
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className='instructionsHeaderDiv'>
+
+          <div>
+            {
+              numSet && numSet.map((num, i) => 
+                <span
+                  key={i}
+                  className='numbersListedSpan'
+                > {num} </span>
+              )
+            }
+            <span className='numbersListedSpan'>= { target }</span>
+          </div>
+        </div>
+
+        <div className='inputAndSolutionContainer'> 
+          <input 
+            value={equation} 
+            onChange={v => {setEquation(v.target.value)}}
+            className='solutionInput'
+          />
+            
+          
+        <div className='solutionDiv'>
+        = { solution || '?' } 
+        </div>
+          
+        </div>
+
+        <button className='validateButton' onClick={validate}>Validate</button>
+
+        <div className='errorMessageDiv'>
+          { errorMessage }
+        </div>
+
+        <div>{formattedTime}</div>
+
+        - Completed Solutions - 
+        {
+          validSolutions && validSolutions.map((s,i) =>
+            <div key={i} >
+              { s }
+            </div>
+          )
+        }
+
+
       </header>
     </div>
   );
