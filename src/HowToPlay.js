be133acfeef399dd22dce91e9101fb0426574a5a
Thomas Dourgarian <Thomasdourgarian@gmail.com>
2023-11-10 12:13:04 -0700

    inital component

---
+++ src/HowToPlay.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Mexp from 'math-expression-evaluator';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+import Confetti from 'react-confetti'
+
+
+function App() {
+  
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
