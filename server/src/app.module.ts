4bd61f9b3929d04cefe0aefe9c0d7e6ffda9ffe0
Thomas Dourgarian <thomasdourgarian@gmail.com>
2023-11-20 14:54:26 -0600

    refresh daily krypto data

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,11 @@ import { KryptoModule } from './krypto/krypto.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { SolutionModule } from './solution/solution.module';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { RefreshKryptoModule } from './refreshKrypto/refreshKrypto.module';
 
 @Module({
-  imports: [ KryptoModule, SolutionModule, PrismaModule, ConfigModule.forRoot({ isGlobal: true }) ],
+  imports: [ RefreshKryptoModule, KryptoModule, SolutionModule, PrismaModule, ConfigModule.forRoot({ isGlobal: true }), ScheduleModule.forRoot() ],
   controllers: [],
   providers: [],
 })
---
+++ server/src/refreshKrypto/refreshKrypto.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class RefreshKryptoService {
+    constructor(private prisma:PrismaService) {}
+
+    generateRandomIntegers(min, max, count) {
+        const randomIntegers = [];
+        for (let i = 0; i < count; i++) {
+            const randomInteger = Math.floor(Math.random() * (max - min + 1)) + min;
+            randomIntegers.push(randomInteger);
+        }
+        return randomIntegers;
+    }
+
+    @Cron('0 0 * * *')
+    async refresh() {
+        const kryptoNumbers = this.generateRandomIntegers(1, 25, 6);
+        const numbersToUse = kryptoNumbers.slice(0, 5).join(' ');
+        const targetNumber = kryptoNumbers[5];
+
+        console.log({
+            numbers: numbersToUse,
+            target: targetNumber,
+        })
+
+
+        try {
+            await this.prisma.daily_krypto.create({
+                data: {
+                numbers: numbersToUse,
+                target: targetNumber,
+                },
+            });
+
+        } catch(error) {
+            console.log('error');
+        }
+    }
+}
