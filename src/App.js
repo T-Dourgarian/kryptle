8f23215feec90682807a9834fd2d021432c67142
Thomas Dourgarian <thomasdourgarian@gmail.com>
2023-11-21 14:45:32 -0600

    bug fixes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import HowToPlay from './HowToPlay/HowToPlay.js';
 
 import GameContainer from './Gameplay/GameContainer';
-import { useDispatch, useSelector } from 'react-redux';
-import { initFromLocalStorage } from './GameSlice';
+import { useSelector } from 'react-redux';
 
 function App() {
   const playedToday = useSelector((state) => state.game.playedToday);
