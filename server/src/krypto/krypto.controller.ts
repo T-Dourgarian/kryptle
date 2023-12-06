31b4172b344dbdcf739d5d0a6a999cb529b0256c
Thomas Dourgarian <thomasdourgarian@gmail.com>
2023-12-06 14:30:16 -0600

    fix protected endpoint

--- server/src/krypto/krypto.controller.ts
+++ server/src/krypto/krypto.controller.ts
@@ -1,17 +1,14 @@
 import { Body, Controller, Post, Get } from "@nestjs/common";
 import { KryptoService } from "./krypto.service";
-// import { AuthDto } from "./dto";
+import { Public } from "src/common/decorators";
 
 @Controller('dailykrypto')
 export class KryptoController {
     constructor(private kryptoService: KryptoService ) {}
 
+    @Public()
     @Get()
     signup(@Body() dto) {
         return this.kryptoService.getDailyKypto(dto);
-    }
-    @Post('signin')
-    signin(@Body() dto){
-        return this.kryptoService.signin(dto);
     }
 }
