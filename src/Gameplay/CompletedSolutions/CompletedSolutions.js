c96d865e41d9032446db13607134eac304342bb4
Thomas Dourgarian <thomasdourgarian@gmail.com>
2023-12-11 20:47:46 -0600

    completed solutions styling

--- src/Gameplay/CompletedSolutions/CompletedSolutions.js
+++ src/Gameplay/CompletedSolutions/CompletedSolutions.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import './CompletedSolutions.css';
+import { Box, Grid, Stack, Typography } from '@mui/joy';
 
 function CompletedSolutions(props) {
   let validSolutions = props.validSolutions;
 
   return (
-    <>
-      <div className="CSLabel">Completed Solutions</div>
-      <div className="CSListContainer">
-        {validSolutions && validSolutions.map((s, i) => <div key={i}>{s}</div>)}
-      </div>
-    </>
+    <Stack>
+      <Typography
+        textAlign={'center'}
+        color='#f3f3f3'
+      >Completed Solutions</Typography>
+      <Stack
+        spacing={1}
+        sx={{
+          height:'200px',
+          overflowY: 'scroll',
+          '&::-webkit-scrollbar': {display: 'none'},
+          backgroundColor:"#737D87FF"
+        }}
+        padding={2}
+        borderRadius={8}
+      >
+        {validSolutions && validSolutions.map((s, i) => 
+          <Box key={i}>
+            {s}
+          </Box>
+        )}
+      </Stack>
+    </Stack>
   );
 }
 
